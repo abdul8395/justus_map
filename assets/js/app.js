dff8682cd33eb0f9e4d915782b0df620fe21075d
unknown <abdulg8395@gmail.com>
2022-12-06 18:21:18 +0500

    full screen added

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,6 +32,18 @@ var googlestreet   = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z=
 
 
 
+    L.control.fullscreen({
+      position: 'topright', // change the position of the button can be topleft, topright, bottomright or bottomleft, default topleft
+      // title: 'Show me the fullscreen !', // change the title of the button, default Full Screen
+      // titleCancel: 'Exit fullscreen mode', // change the title of the button when fullscreen is on, default Exit Full Screen
+      // content: null, // change the content of the button, can be HTML, default null
+      // forceSeparateButton: true, // force separate button to detach from zoom buttons, default false
+      // forcePseudoFullscreen: true, // force use of pseudo full screen even if full screen API is available, default false
+      // fullscreenElement: false // Dom element to render in full screen, false by default, fallback to map._container
+    }).addTo(map);
+
+
+
 
 var baseLayers = {
 "Google Street Map": googlestreet,
@@ -103,6 +115,10 @@ map.on('click', function(e) {
       circlemarker.setStyle({color: 'red'});
   })
 });
+
+
+
+
 
 
 
